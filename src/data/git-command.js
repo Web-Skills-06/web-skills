2f83cca583bd8f540ec76e07dbbd6f07a7812f4b
Kimgeek <baekhajun@cbnu.ac.kr>
2021-09-04 21:39:01 +0900

    feat: Add git-cli-basic contents(#18) git cli command 중 기초적인 내용을 추가했습니다.

--- src/data/git-command.js
+++ src/data/git-command.js
@@ -32,53 +32,71 @@ export const gitCommandCollection = {
         {
             name: "Git-cli",
             skills: [
+                {
+                    name: "Git-cli-basic",
+                    description: {
+                        text: "지옥에서 온 문서 관리자. git을 처음 만든 리누스 토르발스가 남긴 말입니다. 파일이 수없이 변경되는 지옥에서 git은 버전 관리를 하는데 유용한 기술입니다.",
+                        links: [
+                            ["Opentutorials - Git1", "https://opentutorials.org/course/3837"],
+                            ["Pro Git - 공식 문서", "https://git-scm.com/book/ko/v2"],
+                            ["MDN - Git and GitHub", "https://developer.mozilla.org/ko/docs/Learn/Tools_and_testing/GitHub"]
+                        ]
+                    },
+                    skills: [
                         {
-                            name: "Git-cli-basic",
+                            name: "Version",
                             description: {
-                                text: "지옥에서 온 문서 관리자. git을 처음 만든 리누스 토르발스가 남긴 말입니다. 파일이 수없이 변경되는 지옥에서 git은 버전 관리를 하는데 유용한 기술입니다.",
+                                text: "Git을 통해서 프로젝트의 버전 관리를 할 수 있습니다. 아래의 링크는 버전 관리를 실습하는데 이용할 수 있는 강의와 사이트입니다.",
                                 links: [
-                                    ["Opentutorials - Git1", "https://opentutorials.org/course/3837"],
-                                    ["Pro Git - 공식 문서", "https://git-scm.com/book/ko/v2"],
-                                    ["MDN - Git and GitHub", "https://developer.mozilla.org/ko/docs/Learn/Tools_and_testing/GitHub"]
+                                    ["Opentutorials - Version management", "https://opentutorials.org/course/3839"],
+                                    ["Learning Git Branching", "https://learngitbranching.js.org/?locale=ko"],
+                                    ["Git tutorials", "https://violet-bora-lee.github.io/git-tutorial/"],
+                                    ["Git 학습에 도움이 되는 실습 사이트","https://git-school.github.io/visualizing-git/"]
                                 ]
                             },
                             skills: [
                                 {
-                                    name: "Git-version-management",
+                                    name: "Add",
                                     description: {
-                                        text: "소위 Git이라고 하면 command line에서 사용되는 프로그램을 말합니다. 아래의 링크와 강의를 통해 바로 이 프로그램의 사용법을 살펴봅니다.",
+                                        text: "commit을 하기 위해서는 이전에 수정한 파일을 Staging area로 넘기는 작업이 필요합니다. git add 명령어를 사용하면 이제까지 수정한 파일을 일부 혹은 전부 Staging area로 넘길 수 있습니다.",
                                         links: [
-                                            ["Opentutorials - Version management", "https://opentutorials.org/course/3839"],
-                                            ["Learning Git Branching", "https://learngitbranching.js.org/?locale=ko"],
-                                            ["Git tutorials", "https://violet-bora-lee.github.io/git-tutorial/"]
-                                            ["Git 학습에 도움이 되는 실습 사이트","https://git-school.github.io/visualizing-git/"]
+                                            ["Opentutorials - Make a version", "https://opentutorials.org/course/3839/22592"],
+                                            ["Git 공식 문서 - 수정하고 저장소에 저장하기", "https://git-scm.com/book/ko/v2/Git%EC%9D%98-%EA%B8%B0%EC%B4%88-%EC%88%98%EC%A0%95%ED%95%98%EA%B3%A0-%EC%A0%80%EC%9E%A5%EC%86%8C%EC%97%90-%EC%A0%80%EC%9E%A5%ED%95%98%EA%B8%B0"],
                                         ]
                                     }
                                 },
                                 {
-                                    name: "Git commit",
+                                    name: "Commit",
                                     description: {
                                         text: "commit은 git 버전 관리의 최소 단위입니다. add를 통해 staging area로 파일을 옮기고 commit으로 파일을 Repository로 옮깁니다.",
                                         links: [
                                             ["Github - 좋은 커밋 메시지란?", "https://github.com/javascript-tutorial/ko.javascript.info/pull/460"]
                                         ]
                                     },
-                                    skills: [
-                                        {
-                                            name: "",
-                                            description: {
-                                                text: "Learn how to use the alt attribute to provide a useful text alternative to this image.",
-                                                links: [
-                                                    ["Google Devs - Text Alternatives for Images", "https://developers.google.com/web/fundamentals/accessibility/semantics-builtin/text-alternatives-for-images"],
-                                                    ["Webaim - Accessible Images", "https://webaim.org/techniques/images/"],
-                                                    ["Webaim - Alternative text", "https://webaim.org/techniques/alttext/"],
-                                                ]
-                                            }
-                                        }
-                                    ]
+                                },
+                            ]
+                        },
+                        {
+                            name: "Collaboration",
+                            description: {
+                                text: "git은 프로젝트의 버전 관리를 돕기도 하지만 협업에도 중요하게 쓰이는 도구입니다. 이때 협업을 하기 위한 방법이 두 가지 있는데요. 하나는 직접 서버로 돌릴 컴퓨터에 작업한 내용을 옮겨넣는 방법입니다. 하지만 개인이 이 방법을 쓰기엔 현실적으로 어렵습니다. 다른 한 방법이 Github, Gitlab과 같은 원격 저장소를 이용하는 방법입니다. 아래의 링크를 통해 협업하는 방법을 알아보세요.",
+                                links: [
+                                    ["Opentutorials - 협업", "https://opentutorials.org/course/3842"],
+                                    ["Velog - Git 협업 가이드", "https://velog.io/@jinuku/Git-%ED%98%91%EC%97%85-%EA%B0%80%EC%9D%B4%EB%93%9C"]
+                                ]
+                            },
+                            skills: [
+                                {
+                                    name: "Push",
+                                    description: {
+                                        text: "로컬에서 작업을 한 뒤에 원격 저장소로 파일을 올리려면 push를 해야합니다. 아래의 링크를 통해 push를 하면 어떤 일이 일어나는지 한번 알아보세요.",
+                                        links: [
+                                            ["Opentutorial"]
+                                        ]
+                                    },
                                 },
                                 {
-                                    name: "Accessible CSS",
+                                    name: "Pull",
                                     description: {
                                         text: "Learn how to write CSS in such as way that assistive technology better understands it.",
                                         links: [
@@ -89,28 +107,63 @@ export const gitCommandCollection = {
                             ]
                         },
                         {
-                            name: "Git-cli-highlevel",
+                            name: "Reset",
                             description: {
-                                text: "git",
+                                text: "Reset은 최근 작업한 버전을 취소하고 지정한 버전으로 되돌리는 효과를 냅니다. 하지만 엄밀한 정의는 조금 다릅니다. git에서 버전을 삭제하는 일은 웬만하면 드뭅니다. reset의 정확한 기능을 알면 그것을 더 알맞게 활용할 수 있게 될 것입니다.",
                                 links: [
-                                    ["Webaim - Forms", "https://webaim.org/techniques/forms/"],
-                                    ["Itnext - Form Accessibility Gudie", "https://itnext.io/form-accessibility-a-practical-guide-4062b7e2dd14"]
+                                    ["Opentutorials - Reset vs Checkout", "https://opentutorials.org/module/4032/24532"],
+                                    ["Opentutorials - Head의 이해", "https://opentutorials.org/module/4032/24531"],
+                                    ["Git 튜토리얼", "https://violet-bora-lee.github.io/git-tutorial/#reset"]
                                 ]
                             },
                             skills: [
                                 {
-                                    name: "Pull request",
+                                    name: "Options",
                                     description: {
-                                        text: "Pull request는 협업 시 Git에 권한이 없는 타인이 수정사항을 제안하고 그걸 받아들여 내 프로젝트에 병합시킬 때 사용하는 기능입니다. 아래 링크에서 Pull request에 대한 구체적인 사용법을 알아보세요!",
+                                        text: "reset은 최근 작업한 버전을 삭제하고 과거로 되돌리는 기능이 아닙니다. 그와 비슷한 효과를 내지만 더 정확하게는 Head가 가리키는 Branch가 가리키는 버전을 지정해서 바꾸는 것입니다. 아래의 강좌는 reset의 옵션입니다. 옵션에 따라서 Repository, Staging area, Working directory에서 어떤 변화가 일어나는지 알아보세요.",
                                         links: [
-                                            ["Opentutorials - https://opentutorials.org/course/4587"],
-                                            []
+                                            ["Opentutorials - --hard vs --soft vs --mixed", "https://opentutorials.org/module/4032/24533"]
                                         ]
                                     },
-                                }
+                                },
+                                {
+                                    name: "Reset vs Revert",
+                                    description: {
+                                        text: "Reset과 Revert는 프로젝트에서 버전을 지정한 시점으로 되돌리는 기능입니다. 하지만 둘은 기능이나 쓰임새가 다릅니다. 또한, reset은 단순히 버전을 삭제하는 것을 넘어서 프로젝트에 유용하게 쓰일 수 있습니다. 아래 링크의 강좌를 통해 두 명령어를 잘 익히시기 바랍니다.",
+                                        links: [
+                                            ["Opentutorials - Reset vs Revert", "https://opentutorials.org/module/4032/24532"],
+                                            ["Opentutorials - Git reset", "https://opentutorials.org/course/3839/22596"],
+                                            ["Opentutorials - Git revert", "https://opentutorials.org/course/3839/22597"]
+                                        ]
+                                    },
+                                },
                             ]
                         },
-                    ],
-                }
-            ]
-        };
+                    ]
+                },
+                {
+                    name: "Git-cli-highlevel",
+                    description: {
+                        text: "git",
+                        links: [
+                            ["Webaim - Forms", "https://webaim.org/techniques/forms/"],
+                            ["Itnext - Form Accessibility Gudie", "https://itnext.io/form-accessibility-a-practical-guide-4062b7e2dd14"]
+                        ]
+                    },
+                    skills: [
+                        {
+                            name: "Pull request",
+                            description: {
+                                text: "Pull request는 협업 시 Git에 권한이 없는 타인이 수정사항을 제안하고 그걸 받아들여 내 프로젝트에 병합시킬 때 사용하는 기능입니다. 아래 링크에서 Pull request에 대한 구체적인 사용법을 알아보세요!",
+                                links: [
+                                    ["Opentutorials - https://opentutorials.org/course/4587"],
+                                    []
+                                ]
+                            },
+                        }
+                    ]
+                },
+            ],
+        }
+    ]
+};
